a92a84e87bfb475884370d7c0e912d9dd215669c
hthien <leviethoangthien2005@gmail.com>
2025-01-25 21:10:15 +0700

    test lan dau

---
+++ src/flappybirds.java
@@ -0,0 +1,15 @@
+import  javax.swing.*;
+
+public class flappybirds {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 1080;
+        int boardHeight = 720;
+
+        JFrame frame = new JFrame("Flappy Bidr: ");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
